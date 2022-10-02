a30aa74832ae5a29e39803d6bb38717219152194
Gustavo <dorvasgus@gmail.com>
2022-10-02 18:34:25 -0400

    Cliente

--- PRJ_Delivery_Blazor/PRJ_Delivery_Blazor/Program.cs
+++ PRJ_Delivery_Blazor/PRJ_Delivery_Blazor/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PRJ_Delivery_Blazor;
+using PRJ_Delivery_Blazor.Auth;
 using PRJ_Delivery_Blazor.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -15,14 +16,14 @@ builder.Services.AddSingleton(sp => new HttpClient());
 builder.Services.AddConfReposirioHTTP(configuration);
 builder.Services.AddAuthorizationCore();
 
-//builder.Services.AddScoped<ProveedorAutenticacionJWT>();
+builder.Services.AddScoped<ProveedorAutenticacionJWT>();
 
 //builder.Services.AddScoped<IMostrarMensajes, MostrarMensajes>();
 
-//builder.Services.AddScoped<AuthenticationStateProvider, ProveedorAutenticacionJWT>(
-              //provider => provider.GetRequiredService<ProveedorAutenticacionJWT>());
+builder.Services.AddScoped<AuthenticationStateProvider, ProveedorAutenticacionJWT>(
+              provider => provider.GetRequiredService<ProveedorAutenticacionJWT>());
 
-//builder.Services.AddScoped<ILoginService, ProveedorAutenticacionJWT>(
-//              provider => provider.GetRequiredService<ProveedorAutenticacionJWT>());
+builder.Services.AddScoped<ILoginService, ProveedorAutenticacionJWT>(
+              provider => provider.GetRequiredService<ProveedorAutenticacionJWT>());
 
 await builder.Build().RunAsync();
