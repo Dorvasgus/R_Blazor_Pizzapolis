76e82087df54e2f60c5cc664096815663fd2e9ef
Gustavo <dorvasgus@gmail.com>
2022-10-02 16:22:18 -0400

    Post Clientes

--- PRJ_Delivery_Blazor/PRJ_Delivery_Blazor/DTOs/ClienteDTO.cs
+++ PRJ_Delivery_Blazor/PRJ_Delivery_Blazor/DTOs/ClienteDTO.cs
@@ -2,11 +2,7 @@
 {
     public class ClienteDTO
     {
-        public int IdCliente { get; set; }
         public string Nit { get; set; } = null!;
-        public string RazonSocial { get; set; } = null!;
-        public string UserName { get; set; } = null!;
-        public string Password { get; set; } = null!;
-        public int Localidad { get; set; }
+        public int Rol { get; set; }
     }
 }
--- PRJ_Delivery_Blazor/PRJ_Delivery_Blazor/DTOs/ClienteInsertarDTO.cs
+++ PRJ_Delivery_Blazor/PRJ_Delivery_Blazor/DTOs/ClienteInsertarDTO.cs
@@ -4,31 +4,19 @@ namespace PRJ_Delivery_Blazor.DTOs
 {
     public class ClienteInsertarDTO
     {
-        [Required(ErrorMessage = "El campo nombre es obligatorio.")]
-        
-        public int IdCliente { get; set; }
 
         [Required(ErrorMessage = "El campo nombre es obligatorio.")]
         [StringLength(50)]
         public string Nit { get; set; } = null!;
+
         [Required(ErrorMessage = "El campo nombre es obligatorio.")]
-        [StringLength(50)]
-        public string RazonSocial { get; set; } = null!;
-        [Required(ErrorMessage = "El campo nombre es obligatorio.")]
-        [StringLength(50)]
-        public string UserName { get; set; } = null!;
-        [Required(ErrorMessage = "El campo nombre es obligatorio.")]
-        [StringLength(50)]
-        public string Password { get; set; } = null!;
-        [Required(ErrorMessage = "El campo nombre es obligatorio.")]
-        [StringLength(50)]
-        public string Localidad { get; set; } = null!;
+        public int Rol { get; set; }
     }
-    public class ClienteActualizarDTO : ClienteInsertarDTO
-    {
-        [Required]
-        public bool Estado { get; set; }
+    //public class ClienteActualizarDTO : ClienteInsertarDTO
+    //{
+    //    [Required]
+    //    public bool Estado { get; set; }
 
 
-    }
+    //}
 }
